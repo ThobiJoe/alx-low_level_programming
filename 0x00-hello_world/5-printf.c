c2f937c4790227d5369ff23dbb1f894c70fb3bc4
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-16 22:31:53 +0100

    using printf to display results on the standard output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Program that prints out on the standard out put using printf
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
