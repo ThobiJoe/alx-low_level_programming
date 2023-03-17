701d53ac7d2817b876861b859646ef46d4d529a0
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-17 19:54:31 +0100

    Getting last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ * Description: Get the last digit of the output and check if its GT or LT
+ *Return: 0 Always
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int m = n % 10;
+
+	if (m > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	}
+	else if (m == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	}
+	else if (m < 6 && !0)
+	{
+		printf("Last digit of  %d is %d and is less than 6 and not 0\n", n, m);
+	}
+	return (0);
+}
