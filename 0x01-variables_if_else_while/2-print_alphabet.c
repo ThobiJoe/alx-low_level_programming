4d6c6bc21f39d9624a8e9834d17f1d52bb05b1a1
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-17 20:18:53 +0100

    Print A -Z using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *Description: Print out alphabets in small case
+ *Return: 0 always
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 97;
+	while (ch <= 122)
+	{
+		putchar(ch);
+		ch++
+	}
+	putchar("\n");
+	return (0);
+}
