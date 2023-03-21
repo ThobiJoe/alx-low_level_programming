b258fda38f312db10a93df08062996ba762518b1
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-21 23:13:57 +0100

    Sum two values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Sums two integers
+ * @i: first integer to be added
+ * @j: second integer to be added
+ * Return: integer
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
