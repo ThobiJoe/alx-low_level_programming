7b7d89e1c6e32b3c3b096f60352d9590dc5a1745
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-21 20:25:23 +0100

    Check if character is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Entry Point
+ * Description: Check if arguement entered is an alphabet
+ * Return: 1 or 0
+ * @c: Character to be checked
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		if (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
