aeb01718b997bfc17d4ce6343959b47e5009fd28
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-21 19:17:09 +0100

    Print all letters of alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ * Description: Print a-z in lowercase
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	return (0);
+}
