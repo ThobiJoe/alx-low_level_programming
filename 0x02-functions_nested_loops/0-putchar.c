1008411883255c4799e0147048700b3754863f32
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-21 17:56:43 +0100

    My first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Print putchar
+ * Description: Code that displays putchar on the
+ * standard output
+ * Return: 0 always
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
