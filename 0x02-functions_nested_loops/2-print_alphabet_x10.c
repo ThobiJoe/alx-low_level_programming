06ad8fc60eb3d38531cae0a5b676417956f13c80
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-21 20:05:20 +0100

    Print Alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: Print all alphabets 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (i < 10)
+	{
+		c = 'a';
+		while (c < 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		i++;
+	}
+}
