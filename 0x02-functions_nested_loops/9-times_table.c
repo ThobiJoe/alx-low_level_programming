0cb85ef3c5179db62d48701a655a32912d2a9fb6
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-21 22:49:07 +0100

    Correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,14 +5,33 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, k, l, m;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 0; j++)
 		{
-			_putchar(i * j);
+			k = i * j;
+			if (k > 9)
+			{
+				l = k % 10;
+				m = (k - l) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(m - '0');
+				_putchar(l - '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(k - '0');
+			}
 		}
-		_putchar('\n');
+		_putchar('\n)
 	}
 }
