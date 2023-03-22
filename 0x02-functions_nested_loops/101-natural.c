20df5fec624099292f8e3a41303301881bcc2e15
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-22 16:19:43 +0100

    Sum of multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print sum of multiples
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j = j + 1;
+		}
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
