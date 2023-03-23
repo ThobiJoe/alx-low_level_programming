d622d6a7f426c518ae793e45b8b1bd9a4a7c848e
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-23 21:18:27 +0100

    Correction

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * positive_or_negative - Check if arg is pos or neg
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+
+}
