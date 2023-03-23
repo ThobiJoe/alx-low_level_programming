c0dfb215f482d8c152929e37369939b22c9a5231
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-24 00:58:29 +0100

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Function that prints numbers
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
