3494d90fcb0441b20fc22b61b212c316a862d492
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-24 00:36:15 +0100

    Upper or lowercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Check for upper or lower case
+ * @c: The value to be checked
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
