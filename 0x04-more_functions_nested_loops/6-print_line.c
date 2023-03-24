5d1e3050014f4c43e6723fbb63924b124459dd89
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-24 01:49:10 +0100

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Print line n times
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
