c88df026bc69074ea96c57257a9b44fa9e440ce8
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-25 01:13:39 +0100

    Prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Get prime number
+ * Return: 0
+ */
+int main(void)
+{
+	long i, j;
+	long x = 612852475143;
+	double y = sqrt(x);
+
+	for (i = 1; i <= y; i++)
+	{
+		if (x % i == 0)
+		{
+			j = x / i;
+		}
+	}
+	printf("%ld\n", j);
+	return (0);
+}
