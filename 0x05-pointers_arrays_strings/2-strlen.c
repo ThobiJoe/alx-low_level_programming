076a95129b40782cc9135b1d1c6c56c6c6a1f81e
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-27 23:19:01 +0100

    Length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - Return length of string
+ * @s: String to be checked
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	size_t length = strlen(s);
+
+	return (length);
+}
