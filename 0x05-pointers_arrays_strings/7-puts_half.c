93adc1f86c80242d4c98ba1ea6838897452d3328
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-28 00:07:32 +0100

    Print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - Print half of a string
+ * @str: string
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i, j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (j % 2 == 1)
+	{
+		i = (j - 1) / 2;
+		i += 1;
+	}
+	else
+	{
+		i = j / 2;
+	}
+	for (; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
