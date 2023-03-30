3163e5fc6bb5b6d43705265e7efc84fd5477a39c
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-03-30 22:05:39 +0100

    String copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - Copy string
+ * @dest: Input one
+ * @src: input two
+ * @n: input three
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
