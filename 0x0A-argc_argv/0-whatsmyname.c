4793fa38069e6c0fa4400d0232ea5a8440e262f7
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-04-10 17:06:37 +0100

    Correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: array of command line arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
