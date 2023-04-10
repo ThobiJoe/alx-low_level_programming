6b4226aa13c757df2f150b0f928e97c3640f3988
Ifeanyi Ogbonnah <tobixeroz@gmail.com>
2023-04-10 17:00:11 +0100

    First commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Print name of program
+ * @argc: number of content in argv
+ * @argv: array of command line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Working with command line Arguments (Argc and Argv)
